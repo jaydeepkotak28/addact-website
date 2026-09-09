a4ad56059949024c507ceaf3a4198087d5a512f0
jaydeep kotak <jaydeep@addact.net>
2026-09-09 15:37:58 +0530

    BE - SEO
    Created SEO component inside the Site-Setting

--- addact-strapi-be/types/generated/components.d.ts
+++ addact-strapi-be/types/generated/components.d.ts
@@ -1,3 +1,31 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SiteSettingsSeo extends Struct.ComponentSchema {
+  collectionName: 'components_site_settings_seos';
+  info: {
+    displayName: 'SEO';
+    icon: 'book';
+  };
+  attributes: {
+    canonicalURL: Schema.Attribute.Text;
+    languageTag: Schema.Attribute.Enumeration<['en']>;
+    metaDescription: Schema.Attribute.Text;
+    metaRobots: Schema.Attribute.Enumeration<
+      ['index', 'follow', 'noindex', 'nofollow']
+    >;
+    metaTitle: Schema.Attribute.Text;
+    ogDescription: Schema.Attribute.Text;
+    ogImage: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    ogTitle: Schema.Attribute.Text;
+    structuredData: Schema.Attribute.JSON;
+    twitterCardTitle: Schema.Attribute.Text;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export namespace Public {
+    export interface ComponentSchemas {
+      'site-settings.seo': SiteSettingsSeo;
+    }
+  }
+}
