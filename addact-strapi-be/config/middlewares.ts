5a71bea49a239b6f34451d67663246fad5a9653f
jaydeep kotak <jaydeep@addact.net>
2026-09-10 13:16:33 +0530

    BE S3 config
    BE S3 config

--- addact-strapi-be/config/middlewares.ts
+++ addact-strapi-be/config/middlewares.ts
@@ -3,7 +3,38 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'https://d3l7d9gtq0bnch.cloudfront.net',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'https://d3l7d9gtq0bnch.cloudfront.net',
+          ],
+          'script-src': [
+            "'self'",
+            "'unsafe-inline'",
+            "'unsafe-eval'",
+            'https:',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- addact-strapi-be/config/plugins.ts
+++ addact-strapi-be/config/plugins.ts
@@ -23,35 +23,73 @@ const deniedTypes = [
   'application/x-mach-binary',
 ];
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
-  'users-permissions': {
-    config: {
-      jwtManagement: 'refresh',
-      sessions: {
-        httpOnly: true,
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => {
+  const region = env('AWS_REGION');
+  const accessKeyId = env('AWS_ACCESS_KEY_ID');
+  const secretAccessKey = env('AWS_ACCESS_SECRET') || env('AWS_ACCESS_SECRET_KEY') || env('AWS_SECRET_ACCESS_KEY');
+  const bucket = env('AWS_BUCKET') || env('AWS_BUCKET_NAME');
+  const cdnUrl = env('CDN_URL', 'https://d3l7d9gtq0bnch.cloudfront.net');
+
+  return {
+    'users-permissions': {
+      config: {
+        jwtManagement: 'refresh',
+        sessions: {
+          httpOnly: true,
+        },
       },
     },
-  },
-  upload: {
-    config: {
-      security: {
-        allowedTypes: allowedMediaTypes,
-        deniedTypes,
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            region,
+            credentials: {
+              accessKeyId,
+              secretAccessKey,
+            },
+            params: {
+              Bucket: bucket,
+            },
+          },
+          httpOptions: {
+            timeout: 30000,
+          },
+          baseUrl: cdnUrl,
+          cdn: {
+            url: cdnUrl,
+          },
+        },
+        actionOptions: {
+          upload: {
+            beforeUpload(file: any) {
+              file.url = `${cdnUrl}/${file.hash}${file.ext}`;
+              return file;
+            },
+          },
+          uploadStream: {},
+          delete: {},
+        },
+        security: {
+          allowedTypes: allowedMediaTypes,
+          deniedTypes,
+        },
       },
     },
-  },
-  graphql: {
-    config: {
-      endpoint: '/graphql',
-      shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 10,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 10,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
       },
     },
-  },
-});
+  };
+};
 
 export default config;
